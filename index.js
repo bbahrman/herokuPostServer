ff0356df3b1978ef6ccef94420ee3d70d586b3c0
Ben Bahrman <bbahrman@eidebailly.com>
2018-08-02 14:14:43 -0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app
+  .use(bodyParser.json())
+  .post('/', onPost)
+  .listen(PORT, () => console.log(`Listening on ${ PORT }`));
+
+
+function onPost(req, res) {
+  console.log(req.body);
+}
